1a6a055c5f8e663f5aa61efe170ca0c182eb70c1
Artsiom <artyom.stan0905@gmail.com>
2025-03-01 13:45:18 +0300

    feat: lower error count

--- src/features/widget-data/useMatches.ts
+++ src/features/widget-data/useMatches.ts
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 import { create } from "zustand";
 
-const ERROR_COUNT_TO_CHANGE_PROVIDER = 3;
+const ERROR_COUNT_TO_CHANGE_PROVIDER = 1;
 
 const useErrorsStore = create<{
   errors: number;
